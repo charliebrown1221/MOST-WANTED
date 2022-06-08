e5b701c1997f915c2bbb5bf1a3df4ea1e48d0c6d
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-08 17:06:39 -0400

    finshed function displayPerson(info) + tested

--- app.js
+++ app.js
@@ -142,7 +142,14 @@ function displayPeople(people) {
  */
 function displayPerson(person) {
     let personInfo = `First Name: ${person.firstName}\n`;
-    personInfo += `Last Name: ${person.lastName}\n`;
+    personInfo += `Gender: ${person.lastName}\n`;
+    personInfo += `Dob: ${person.dob}\n`;
+    personInfo += `Height: ${person.height}\n`;
+    personInfo += `Weight: ${person.weight}\n`;
+    personInfo += `EyeColor: ${person.eyeColor}\n`;
+    personInfo += `Occupation: ${person.occupation}\n`;
+    
+
     //! TODO #1a: finish getting the rest of the information to display //////////////////////////////////////////
     alert(personInfo);
 }
@@ -274,7 +281,6 @@ function searchByHeight(people){
             if( Number(userInput) === person.height){
                 return true;
             }
-
 
         }
     );
